f3c10a738fa86c0df12cbb47b8e96daa5ae32b91
Josh Horwitz <jahorwitz94@gmail.com>
2023-09-10 14:15:14 -0400

    fix: migration issue

--- migrations/20230910181429_initial_migration/migration.sql
+++ migrations/20230910181429_initial_migration/migration.sql
@@ -22,13 +22,22 @@ CREATE TABLE `Post` (
     `title` VARCHAR(191) NOT NULL DEFAULT '',
     `content` JSON NOT NULL,
     `author` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NULL,
 
     INDEX `Post_author_idx`(`author`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Tag` (
+CREATE TABLE `Keyword` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL DEFAULT '',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Slop` (
     `id` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NOT NULL DEFAULT '',
 
@@ -53,19 +62,34 @@ CREATE TABLE `Movie` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `_Post_tags` (
+CREATE TABLE `_Post_slops` (
     `A` VARCHAR(191) NOT NULL,
     `B` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `_Post_tags_AB_unique`(`A`, `B`),
-    INDEX `_Post_tags_B_index`(`B`)
+    UNIQUE INDEX `_Post_slops_AB_unique`(`A`, `B`),
+    INDEX `_Post_slops_B_index`(`B`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `_Keyword_posts` (
+    `A` VARCHAR(191) NOT NULL,
+    `B` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `_Keyword_posts_AB_unique`(`A`, `B`),
+    INDEX `_Keyword_posts_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
 ALTER TABLE `Post` ADD CONSTRAINT `Post_author_fkey` FOREIGN KEY (`author`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_Post_tags` ADD CONSTRAINT `_Post_tags_A_fkey` FOREIGN KEY (`A`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `_Post_slops` ADD CONSTRAINT `_Post_slops_A_fkey` FOREIGN KEY (`A`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `_Post_tags` ADD CONSTRAINT `_Post_tags_B_fkey` FOREIGN KEY (`B`) REFERENCES `Tag`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `_Post_slops` ADD CONSTRAINT `_Post_slops_B_fkey` FOREIGN KEY (`B`) REFERENCES `Slop`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_Keyword_posts` ADD CONSTRAINT `_Keyword_posts_A_fkey` FOREIGN KEY (`A`) REFERENCES `Keyword`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `_Keyword_posts` ADD CONSTRAINT `_Keyword_posts_B_fkey` FOREIGN KEY (`B`) REFERENCES `Post`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
