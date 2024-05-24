e9ca51a02bfd53b68b0703e662c0b4a3561926ee
Daniel Marzahl <marzahlde09@gmail.com>
2024-05-23 19:49:47 -0500

    fix: reduce migrations to one file

--- migrations/20240524004842_add_sound_model/migration.sql
+++ migrations/20240524004842_add_sound_model/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE `Sound` (
     `id` VARCHAR(191) NOT NULL,
-    `author` VARCHAR(191) NULL,
     `title` VARCHAR(191) NOT NULL DEFAULT '',
     `photo_filesize` INTEGER NULL,
     `photo_extension` VARCHAR(191) NULL,
@@ -11,7 +10,6 @@ CREATE TABLE `Sound` (
     `audio_filesize` INTEGER NULL,
     `audio_filename` VARCHAR(191) NULL,
 
-    INDEX `Sound_author_idx`(`author`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -23,9 +21,6 @@ CREATE TABLE `_Movie_sounds` (
     UNIQUE INDEX `_Movie_sounds_AB_unique`(`A`, `B`),
     INDEX `_Movie_sounds_B_index`(`B`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Sound` ADD CONSTRAINT `Sound_author_fkey` FOREIGN KEY (`author`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `_Movie_sounds` ADD CONSTRAINT `_Movie_sounds_A_fkey` FOREIGN KEY (`A`) REFERENCES `Movie`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
