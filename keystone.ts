1cba34cd59987a3614cc65ffc856f74db8a787ed
Danielle Foss <126746048+dani9401@users.noreply.github.com>
2024-06-02 16:06:06 -0600

    feat: pass s3Upload to response. More code cleanup

--- keystone.ts
+++ keystone.ts
@@ -5,7 +5,7 @@ import { uploadFile } from "./s3";
 dotenv.config();
 import * as Models from "./models";
 import { withAuth, session } from "./auth";
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+//const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 const multer = require("multer");
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -30,7 +30,7 @@ export default withAuth(
           async (req, res) => {
             const userId = req.body.userId;
             //confirm userId exists in database
-            //probably need to use withAuth for this
+            //probably need to use withAuth for this?
 
             const fileData = {
               movieTitle: req.body.movieTitle,
@@ -38,40 +38,8 @@ export default withAuth(
             };
 
             const s3upload = await uploadFile(fileData);
-            console.log("s3upload", s3upload);
 
-            //after calling upload file we call graphQL to insert movie into db
-            //find keystone examples
-
-            //const context = await commonContext.withRequest(req, res);
-            //work on figuring out access issues from here
-
-            //if (!context.session) return res.status(401).end()
-            //const isDraft = req.query?.draft === "1";
-            // const tasks = await context.query.Movie.createOne(
-            //       //gets us access to graphql
-            //       {
-            //         data: {
-            //           author: {
-            //             connect: {
-            //               id: req.body.userId,
-            //               username: req.body.username
-            //             }
-            //           },
-            //           title: req.body.title,
-            //           description: req.body.description,
-            //           //releaseYear: req.body.releaseYear,
-            //           //runtime: req.body.runtime,
-            //           photo: null,
-            //           //tomatoScore: req.body.tomatoScore,
-            //           howToWatch: "",
-            //           keywords: {},
-            //         },
-            //       },
-            //     );
-            //     console.log(tasks);
-
-            res.json({ success: true });
+            res.json(s3upload);
           },
         );
       },
@@ -116,60 +84,6 @@ export default withAuth(
     session,
   }),
 );
-
-// *******Do I even need this? If yes, does it go in this file?
-// export const context = {
-//     Query API
-//     query,
-
-//     Database API
-//   db,
-
-//     HTTP request object
-//     req,
-//     res,
-
-//     GraphQL helpers
-//   graphql: {
-//     schema,
-//     run,
-//     raw,
-//   },
-
-//   New context creators
-//   sudo,
-//   withRequest,
-//   withSession,
-
-//     Raw Prisma access
-//     prisma,
-
-//     Images API
-// images: {
-//   **************research more to find appropriate option
-//   getUrl,
-//   getDataFromRef,
-//   getDataFromStream,
-// },
-// }
-
-// ***** example for extendHttpServer settings...
-
-// import { WebSocketServer } from 'ws';
-// import { useServer as wsUseServer } from 'graphql-ws/lib/use/ws';
-
-// export default config<TypeInfo>({
-//   server: {
-//     extendHttpServer: (httpServer, commonContext) => {
-//       const wss = new WebSocketServer({
-//         server: httpServer,
-//         path: '/api/graphql',
-//       });
-
-//       wsUseServer({ schema: commonContext.graphql.schema }, wss);
-//     },
-//   },
-// });
 
 //***********Images API
 //If support for image fields is enabled in the system,
